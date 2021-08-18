19fe89133737fa539e136f5fa886ef8406c435f7
rene rodriguez <reneclavijo@live.com>
2021-08-18 14:01:55 -0500

    agregando la configuración de I18n

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Oficinas
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    I18n.available_locales = [:es, :en]
+    I18n.default_locale = :es
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+
   end
 end
