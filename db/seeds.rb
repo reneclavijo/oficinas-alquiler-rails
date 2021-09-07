adcde85f74fcdff2837053a2cd70a16fa378d45c
rene rodriguez <reneclavijo@live.com>
2021-09-07 16:36:22 -0500

    agregando semilla

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Administrador.delete_all
+Administrador.create({ nombre_usuario: 'felipe', password: 'qwerty'})
